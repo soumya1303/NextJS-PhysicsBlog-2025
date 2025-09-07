1f33e45aadbcc4b9def0fe55fad1dbc62d982e16
soumya1303 <soumya_rbs@yahoo.co.in>
2025-09-07 16:18:16 +0100

    added api route v1.002

--- pages/api/postComment.js
+++ pages/api/postComment.js
@@ -2,14 +2,14 @@ import { MongoClient } from "mongodb";
 
 const handle=async (req, res)=>{
 
-    const uri="mongodb+srv://db-user:password0@cluster0.9sys7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    const uri=`${process.env.mongo_conn_str}`
 
     const client=new MongoClient(uri);
 
     try {
         const conn=await client.connect();    
         
-        const _db=conn.db("fs25DB");
+        const _db=conn.db(`${process.env.mongo_schema}`);
 
         const _collection=_db.collection("techblogcomment");
 
